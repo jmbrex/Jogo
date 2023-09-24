0e83953c53e3ec4f9a95e00c626479b1de22f2fb
João Marcelo <145791198+jmbrex@users.noreply.github.com>
2023-09-24 19:35:15 -0300

    Arquivos atualizados

--- src/main/java/ads/jogorpg/DataBase/DbSQL.java
+++ src/main/java/ads/jogorpg/DataBase/DbSQL.java
@@ -10,8 +10,8 @@ public class DbSQL {
     private static final String usuario = "root";
     private static final String senha = "";
     
+    //CRIANDO CONEXÃO COM MYSQL
     public Connection connectionMySql(){
-        
         try{
             conexao_MySql = DriverManager.getConnection(LINK, usuario, senha);
             System.out.println("Conexao OK!");
@@ -21,7 +21,7 @@ public class DbSQL {
         }
         return conexao_MySql;
     }
-    
+    // FECHANDO CONEXÃO COM MYSQL
     public void closeConnectionMySql(Connection con){
         try{
             if(con != null){
@@ -33,6 +33,7 @@ public class DbSQL {
         }
     }
     
+    //INSERINDO USUARIO NA TEBELA USUARIO
     public void sqlDbUserInsert(User user){
         Connection connection = connectionMySql();
         
@@ -49,6 +50,33 @@ public class DbSQL {
         }catch(SQLException e){
             e.printStackTrace();}
         closeConnectionMySql(connection);
+    }
+    //SELECIONANDO USUARIO PELO NICKNAME
+    public User sqlUserSelectNickName(String NickName){
+        User p1 = new User();
+        
+        Connection connection = connectionMySql();
+        String StringSQL = "Select * from usuario where NickName  = ?";
+        PreparedStatement preparedStmt;
+        
+        try{
+            preparedStmt = connection.prepareStatement(StringSQL);
+            preparedStmt.setString(1,NickName);
+            ResultSet result = preparedStmt.executeQuery();
+            while(result.next()){
+                p1.setID(result.getInt("ID"));
+                p1.setNickName(result.getString("NickName"));
+                p1.setEmail(result.getString("email"));
+                p1.setUserName(result.getString("UserName"));
+                p1.setSex(result.getString("Sex"));
+                p1.setPassWord(result.getString("Senha"));
+            }
+        }catch  (SQLException e){
+            e.printStackTrace();
+        }
+        closeConnectionMySql(connection);
+        
+        return p1;
     }
     
     public void sqlDbPersonagemInsert(Personagens personagem){
