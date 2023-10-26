1bb6c172dc637fbe1447788e95290fa59431e722
João Marcelo <145791198+jmbrex@users.noreply.github.com>
2023-10-25 22:27:39 -0300

    Add files via upload

--- src/main/java/ads/jogorpg/Batalha/Ataques.java
+++ src/main/java/ads/jogorpg/Batalha/Ataques.java
@@ -8,20 +8,21 @@ public class Ataques {
     
     
     
-    private void Confronto(Personagens P1, Inimigos I1){
+    public boolean ResultadoConfronto(Personagens P1, Inimigos I1){
         Ataques C1 = new Ataques();
         
         if(I1.getVida()<=1){
             P1.setInimigosDerrotados(P1.getInimigosDerrotados()+1);
             JOptionPane.showMessageDialog(null, P1.getName()+" Derrotou um Oponente");
+            return true;
         }
-        
+        return false;
     }
     
     public int AtaqueLeve(Personagens personagem, Inimigos inimigo){
         int x = 0;
         Random gerador = new Random();
-        
+        JOptionPane.showMessageDialog(null, personagem.getName()+" ataque Leve!");
         if(personagem.getClasse().equals("Guerreiro") || personagem.getClasse().equals("arqueiro")){
             if(personagem.getStregth() > inimigo.getDefense()){
                 if(personagem.getCritical() > gerador.nextInt(100)){
@@ -35,12 +36,13 @@ public class Ataques {
                     personagem.setStamina(personagem.getStamina() - 20);
                 }
             }else{
-                JOptionPane.showMessageDialog(null, inimigo.getName()+": Ataque muito frco");
+                JOptionPane.showMessageDialog(null, personagem.getName()+": Ataque muito frco");
                 personagem.setStamina(personagem.getStamina() - 10);
             }
+            
         }
         
-        if(personagem.getClasse().equals("mago")){
+        if(personagem.getClasse().equals("Mago")){
             if(personagem.getMagic()> inimigo.getDefense()){
                 if(personagem.getCritical() > gerador.nextInt(100)){
                     x = (personagem.getMagic()*2)- inimigo.getDefense();
@@ -57,17 +59,16 @@ public class Ataques {
                 personagem.setStamina(personagem.getStamina() - 15);
             }
         }
-        System.out.println(personagem.getClasse());
-        System.out.println(personagem.getStregth());
-        System.out.println(inimigo.getVida());
-        this.Confronto(personagem, inimigo);
+       
+       
+            JOptionPane.showMessageDialog(null, "Dano Causado "+x); 
         return x;
     }
     
     public int AtaquePesado(Personagens personagem, Inimigos inimigo){
         int x = 0;
         Random gerador = new Random();
-        
+        JOptionPane.showMessageDialog(null, personagem.getName()+" ataque Pesado!");
         if(personagem.getClasse().equals("Guerreiro") || personagem.getClasse().equals("arqueiro")){
             if(personagem.getStregth() > inimigo.getDefense()){
                 if(personagem.getCritical() > gerador.nextInt(100)){
@@ -103,7 +104,8 @@ public class Ataques {
                 personagem.setStamina(personagem.getStamina() - 20);
             }
         }
-        this.Confronto(personagem, inimigo);
+        
+        JOptionPane.showMessageDialog(null, "Dano Causado "+x);
         return x;
     }
     
